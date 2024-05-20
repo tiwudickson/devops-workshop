000bf7e1ee38d74ce8e0fc167975c1775ff3c03d
tiwudickson <akwadickson001@gmail.com>
2024-05-20 15:09:29 -0500

    commiting

--- terraform_code/V2-EC2.tf
+++ terraform_code/V2-EC2.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
+resource "aws_instance" "damo-server" {
+    ami = "ami-0bb84b8ffd87024d8"
     instance_type = "t2.micro"
     key_name = "dpp"
     security_groups = [ "demo-sg" ]
@@ -12,25 +12,25 @@ resource "aws_instance" "demo-server" {
 resource "aws_security_group" "demo-sg" {
   name        = "demo-sg"
   description = "SSH Access"
-  
-  ingress {
-    description      = "Shh access"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    }
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
 
   tags = {
-    Name = "ssh-prot"
-
+    Name = "ssh-port"
   }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "demo-sg" {
+  security_group_id = aws_security_group.demo-sg.id
+  cidr_ipv4         = ["0.0.0.0/0"]
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "demo-sg" {
+  security_group_id = aws_security_group.demo-sg.id
+  from_port = 0
+  to_port =  0
+  cidr_ipv4         = ["0.0.0.0/0"]
+  ip_protocol       = "-1" # semantically equivalent to all ports
 }
